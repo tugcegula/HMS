911ab94f5c92085005c942cda89fea6068b0803b
tugcegula <tugcegulacar@gmail.com>
2024-11-01 01:02:17 +0300

    Medical Records sınıfı eklendi.

--- src/main/java/com/example/entity/MedicalRecords.java
+++ src/main/java/com/example/entity/MedicalRecords.java
@@ -14,29 +14,65 @@ public class MedicalRecords {
     @Column(name= "recordId")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long recordId;
+
     @ManyToOne
     @JoinColumn(name = "patients_id" , referencedColumnName = "patientsId")
     @JsonBackReference
     private Patients patients;
 
 //Doktor eklenince
-//    @OneToMany(mappedBy ="")
-//    @JsonManagedReference
-//    private List<MedicalRecords> records;
-//@ManyToOne
-//@JoinColumn(name = "" , referencedColumnName = "")
-//@JsonBackReference
-//private ;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id" , referencedColumnName = "doctorId")
+    @JsonBackReference
+    private Doctor doctor;
 
 
     //Reçete eklenince eklenince
-//    @OneToMany(mappedBy ="")
-//    @JsonManagedReference
-//    private List<MedicalRecords> records;
+    @OneToMany(mappedBy ="medicalrecords")
+    @JsonManagedReference
+    private List<Prescription> prescriptions;
 //@ManyToOne
-//@JoinColumn(name = "" , referencedColumnName = "")
-//@JsonBackReference
-//private ;
+////@JoinColumn(name = "" , referencedColumnName = "")
+////@JsonBackReference
+////private ;
 
+    public MedicalRecords(Long recordId, Patients patients, Doctor doctor, List<Prescription> prescriptions) {
+        this.recordId = recordId;
+        this.patients = patients;
+        this.doctor = doctor;
+        this.prescriptions = prescriptions;
+    }
 
+    public Long getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(Long recordId) {
+        this.recordId = recordId;
+    }
+
+    public Patients getPatients() {
+        return patients;
+    }
+
+    public void setPatients(Patients patients) {
+        this.patients = patients;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public List<Prescription> getPrescriptions() {
+        return prescriptions;
+    }
+
+    public void setPrescriptions(List<Prescription> prescriptions) {
+        this.prescriptions = prescriptions;
+    }
 }
